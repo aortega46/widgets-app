68741de815da3e556ef9eacac33dfe00c0dc2650
Albert Ortega <trebla.og46@gmail.com>
2023-07-13 11:43:49 +0200

    theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+        assert(selectedColor < colorList.length, 'Selected color must be lower than ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+      );
+}
