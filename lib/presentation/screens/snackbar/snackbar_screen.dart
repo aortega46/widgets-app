95b2dc51ff641b56efb3665669d98d73548d3a90
Albert Ortega <trebla.og46@gmail.com>
2023-07-13 23:23:13 +0200

    diálogos terminado

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -17,11 +18,60 @@ class SnackbarScreen extends StatelessWidget {
     );
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás seguro?'),
+        content: const Text(
+            'Consequat pariatur voluptate amet consectetur ad cillum nostrud eu excepteur.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: const Text('Cancelar'),
+          ),
+          FilledButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: const Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y Diálogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text(
+                        'Laborum Lorem proident nisi consectetur enim nisi. Aliqua adipisicing quis deserunt elit anim duis esse qui. Eiusmod consequat aliquip dolore ad minim pariatur enim do. Aliqua officia ipsum eiusmod sunt eu tempor amet veniam. Enim aute amet velit labore velit do nostrud cillum in reprehenderit esse commodo. Eiusmod adipisicing ex deserunt labore minim cillum. Adipisicing mollit dolor ea incididunt laborum id incididunt laboris laboris.')
+                  ],
+                );
+              },
+              child: const Text('Licencias usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar diálogo'),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => showCustomSnackbar(context),
