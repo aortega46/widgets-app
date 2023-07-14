5dcc91b1c641ef34d0c6fad313d0b494bdafed2b
Albert Ortega <trebla.og46@gmail.com>
2023-07-14 16:12:50 +0200

    riverpod screen

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -16,6 +16,12 @@ class MenuItem {
 
 const appMenuItems = <MenuItem>[
   MenuItem(
+    title: 'Riverpod Counter',
+    subTitle: 'Introducción a Riverpod',
+    link: '/counter',
+    icon: Icons.add,
+  ),
+  MenuItem(
     title: 'Botones',
     subTitle: 'Varios botones en Flutter',
     link: '/buttons',
